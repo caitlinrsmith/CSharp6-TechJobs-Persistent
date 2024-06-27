dc7a2506a0c052ad5a54f2cadebc55040b0bb290
caitlinrsmith <catie12smith@gmail.com>
2024-06-26 23:59:01 -0500

    task 1 done

--- TechJobs6Persistent/queries.sql
+++ TechJobs6Persistent/queries.sql
@@ -4,6 +4,8 @@
 
 --Part 1: List the columns and their data types in the Jobs table.
 
+-- 
+
 --Part 2: Write a query to list the names of the employers in St. Louis City.
 
 --Part 3: Write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
